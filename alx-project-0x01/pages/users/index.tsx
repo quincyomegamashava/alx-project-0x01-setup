43c82d680c706b436e4cf5eb4c7c05f4866b6f80
QuincyMashava <mashavaquincy@gmail.com>
2025-09-28 13:27:18 +0200

    feat: fixed task error

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -12,7 +12,7 @@ const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts?.map((u) => (
+          {posts.map((u) => (
             <UserCard key={u.id} {...u} />
           ))}
         </div>
